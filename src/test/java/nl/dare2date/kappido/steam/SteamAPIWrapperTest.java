3213e7c7fbdb07debf94d90677c8048d928aacde
Olle Kelderman <omkelderman@gmail.com>
2015-10-05 14:20:18 +0200

    derp, within UnitTest the real api-key is not needed at all

--- src/test/java/nl/dare2date/kappido/steam/SteamAPIWrapperTest.java
+++ src/test/java/nl/dare2date/kappido/steam/SteamAPIWrapperTest.java
@@ -3,10 +3,6 @@ package nl.dare2date.kappido.steam;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -18,20 +14,12 @@ import static org.junit.Assert.assertTrue;
 public class SteamAPIWrapperTest {
     private static final String MINEMAARTEN_STEAM_ID = "76561198034641265";
     private static final String GARRYS_MOD_APP_ID = "4000";
-    private static final String STEAM_API_PATH = "SteamAPIKey.txt"; //Within the resources folder.
 
     SteamAPIWrapper steamAPIWrapper;
 
     @Before
     public void init() {
-        final String apiKey;
-        InputStream inputStream = ClassLoader.getSystemResourceAsStream(STEAM_API_PATH);
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
-            apiKey = br.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e); // TODO handle properly
-        }
-        steamAPIWrapper = new SteamAPIWrapper(apiKey, new FakeSteamURLResourceProvider());
+        steamAPIWrapper = new SteamAPIWrapper("steamapikey", new FakeSteamURLResourceProvider());
     }
 
     @Test
